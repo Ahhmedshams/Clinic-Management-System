d1bd09d393f8546d750798c91402ec6e0884b51f
Ahhmedshams <ahmedtaha757@gmail.com>
2023-01-30 10:46:40 +0200

    Intial Setup

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
-const express = require("express");
+require('dotenv').config();
+const express= require("express");
+const morgan =require("morgan");
+const mongoose = require("mongoose");
 
-//open server with http protocol  http://localhost:8080
+
+//Server
 const server = express();
 
-let port = process.env.PORT || 8080;
 
-server.listen(port, () => {
-    console.log("I'm Listening........", port);
-});
+let port = process.env.PORT || 8080;
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.DB_URL)
+        .then(()=>{
+            server.listen(port,()=>{
+                console.log(`Server Is Running In ${process.env.DB_URL} On Port ${port}`)
+            });
+        })
+        .catch(error=>{
+            console.log(" BD Problem" + error)
+        })
+//morgan Middleware
+server.use(morgan('tiny'))
+//convert Data to Json format
+server.use(express.json())
+
+//Routes
+
+
+//Not Found Middleware
+server.use((request,response,next)=>{
+    response.status(404).json({data:"Not Found"});
+})
+//Error MiddleWare
+server.use((error,request,response,next)=>{
+    response.status(500).json({message:`Error ${error}`});
+})
