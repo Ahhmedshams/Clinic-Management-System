1e443ba886df964cb196d4b17ca045968f1af62a
Ahmed-Elagezy <55804919+ahmed-elagezy@users.noreply.github.com>
2023-02-08 20:25:48 +0200

    Delete routes directory

--- routes/login.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const loginController = require("./../Controllers/loginController");
-
-const router = express.Router();
-
-router.route("/login").post(loginController.login);
-
-module.exports = router;
