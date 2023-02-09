c33c15590fd5b90bc7034f372fe87409032afb0f
Abdullah <56775516+Zaazou@users.noreply.github.com>
2023-02-09 05:49:12 +0200

    all

--- routes/clinic.js
+++ routes/clinic.js
@@ -14,7 +14,6 @@ router.route("/clinic")
     .get(advancedResults(clinic), controller.getAllclinics)
     .post(expressValidation.clinicPost, validator, controller.addClinic)
 
-
 router.route("/clinic/:id")
     .get(expressValidation.paramIdInt, validator, controller.getClinicByID)
     .delete(expressValidation.paramIdInt, validator, controller.deleteClinicByID)
